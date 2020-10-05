73e02d383c912c81ada2141ddb89a5ade210aa19
youngmin-oh <objconsolelog@gmail.com>
2020-10-05 18:49:10 +0900

    new file upload

---
+++ front/components/DeathCntChart.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { Line } from "react-chartjs-2";
+
+const DeathCntChart = () => {
+  let date = [];
+  let cur = [];
+  let data;
+  const items = useSelector((state) => state.covid.chartData);
+
+  for (let index in items) {
+    date[index] = items[index].stateDt;
+    cur[index] = items[index].deathCnt;
+  }
+  data = {
+    labels: date,
+    datasets: [
+      {
+        label: "누적 확진자 수",
+        data: cur,
+        fill: false,
+        pointHitRadius: 10,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+    ],
+  };
+
+  return (
+    <div className="chartApp">
+      <Line data={data} />
+    </div>
+  );
+};
+
+export default DeathCntChart;
